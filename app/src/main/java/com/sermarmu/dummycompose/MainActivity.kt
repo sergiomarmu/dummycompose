600f3ad0bc6b42b131d5ce8ebaff9d74556254fb
Sergio Martinez Mulero <sergiomarmu@gmail.com>
2021-08-11 17:23:18 +0200

    init commit

---
+++ app/src/main/java/com/sermarmu/dummycompose/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.sermarmu.dummycompose
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
